3e4969807fd67e8d9d5ae73fc60a2d9459cb90ec
Claudio Barbera <barbera.claudio@gmail.com>
2020-07-16 11:32:16 +0200

    Move logic to load an image in UIImageView extension

--- IQUIIGalleryApp/Extensions/UIImageView+Extensions.swift
+++ IQUIIGalleryApp/Extensions/UIImageView+Extensions.swift
@@ -9,9 +9,43 @@
 import UIKit
 
 extension UIImageView {
+    
     func transition(to image: UIImage) {
         UIView.transition(with: self, duration: 0.3, options: [.transitionCrossDissolve], animations: {
             self.image = image
         }, completion: nil)
+    }
+    
+    func loadImage(from url: URL?, placeholder: UIImage? = UIImage(named: "placeholder"), completion: (() -> Void)? = nil) {
+        guard let url = url else { return }
+        
+        let request = URLRequest(url: url)
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let data = URLCache.shared.cachedResponse(for: request)?.data , let image = UIImage(data: data) {
+                
+                DispatchQueue.main.async { [weak self] in
+                    self?.transition(to: image)
+                    completion?()
+                }
+            } else {
+                URLSession.shared.dataTask(with: request) { data, response, error in
+                    if let data = data, let response = response, let image = UIImage(data: data) {
+                        let cachedData = CachedURLResponse(response: response, data: data)
+                        URLCache.shared.storeCachedResponse(cachedData, for: request)
+                        
+                        DispatchQueue.main.async { [weak self] in
+                            self?.transition(to: image)
+                            completion?()
+                        }
+                    } else if let placeholder = placeholder {
+                        DispatchQueue.main.async { [weak self] in
+                            self?.transition(to: placeholder)
+                            completion?()
+                        }
+                    }
+                }.resume()
+            }
+        }
     }
 }
