91f28dd6a076dc11c0f7cd5e1ab17425554fc934
Claudio Barbera <barbera.claudio@gmail.com>
2020-07-16 11:33:17 +0200

    Added details view controller

--- IQUIIGalleryApp/Scenes/Details/PostDetailsViewController.swift
+++ IQUIIGalleryApp/Scenes/Details/PostDetailsViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class PostDetailsViewController: UIViewController {
- 
+    
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     // MARK: - Vars
     private let post: Post
     
@@ -27,7 +31,75 @@ class PostDetailsViewController: UIViewController {
     // MARK: - View controller
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.loadImage(from: post.thumbnail) { [weak self] in
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        if let image = imageView.image {
+            setMinZoomScaleForImageSize(image.size)
+        }
+    }
+    
+    @IBAction func onImageTap(_ sender: UITapGestureRecognizer) {
+        if scrollView.zoomScale == scrollView.minimumZoomScale {
+            scrollView.zoom(to: zoomRectangle(scale: scrollView.maximumZoomScale, center: sender.location(in: sender.view)), animated: true)
+        } else {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        }
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension PostDetailsViewController {
+    
+    private func setMinZoomScaleForImageSize(_ imageSize: CGSize) {
+        let widthScale = view.frame.width / imageSize.width
+        let heightScale = view.frame.height / imageSize.height
+        let minScale = min(widthScale, heightScale)
+        
+        // Scale the image down to fit in the view
+        scrollView.minimumZoomScale = minScale
+        scrollView.zoomScale = minScale
+        
+        // Set the image frame size after scaling down
+        let imageWidth = imageSize.width * minScale
+        let imageHeight = imageSize.height * minScale
+        let newImageFrame = CGRect(x: 0, y: 0, width: imageWidth, height: imageHeight)
+        imageView.frame = newImageFrame
+        
+        centerImage()
+    }
+    
+    private func centerImage() {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = view.frame.size
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+    }
+    
+    private func zoomRectangle(scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        zoomRect.size.height = imageView.frame.size.height / scale
+        zoomRect.size.width  = imageView.frame.size.width  / scale
+        zoomRect.origin.x = center.x - (center.x * scrollView.zoomScale)
+        zoomRect.origin.y = center.y - (center.y * scrollView.zoomScale)
+        
+        return zoomRect
+    }
+}
+
+extension PostDetailsViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerImage()
     }
 }
